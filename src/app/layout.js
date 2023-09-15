291abad0261bab493ea4dbc8b9d37115f5086363
Rohit Nag <rnag2018@gmail.com>
2023-09-15 08:47:05 +0530

    Changed favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,8 @@
 import './globals.scss'
-import { Inter, Raleway, Montserrat, Open_Sans, Lato } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import { Navbar, Footer } from '@/components/layout'
 
-const inter = Inter({ subsets: ['latin'] })
-const raleway = Raleway({ subsets: ['latin'] })
 const montserrat = Montserrat({ subsets: ['latin'] })
-const open_sans = Open_Sans({ subsets: ['latin'] })
-const lato = Lato({ subsets: ['latin'], weight: ['100', '300', '400', '700'], })
 
 export const metadata = {
   title: 'OSSTEC',
