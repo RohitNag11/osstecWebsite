182471a14b047f9ed87dfa938b45e9a40acf60e1
Rohit Nag <rnag2018@gmail.com>
2023-09-15 09:08:53 +0530

    Updated meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,28 @@ import { Navbar, Footer } from '@/components/layout'
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'OSSTEC',
+  title: {
+    template: '%s | OSSTEC',
+    default: 'OSSTEC',
+  },
   description: 'Additive Manufactured Orthopedic Implants',
+  icons: {
+    icon: '/icons/favicon.ico',
+    apple: '/icons/apple-icon.png',
+  },
+  generator: 'Next.js',
+  applicationName: 'OSSTEC Website',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Implants', 'Orthopaedics', '3D Printing', 'Additive Manufacturing', 'Knee Replacement', 'Partial Knee Replacement', 'Knee Implant', 'Orthopaedic Implant', 'Imperial College London', 'Start-up', 'Spin-out', 'Spin-out Company', 'London', 'UK'],
+  authors: [{ name: 'Rohit Nag', url: 'https://rohitnag.com' }],
+  colorScheme: 'light',
+  creator: 'Rohit Nag',
+  publisher: 'OSSTEC',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
 }
 
 export default function RootLayout({ children }) {
